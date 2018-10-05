2f04cd9cb4753f8903ecab0e65719712fa9ce8a6
Steven Gajek <sgajek@hotmail.com>
2018-10-05 21:13:00 +0100

    Updates

--- uninstallscript.sh
+++ uninstallscript.sh
@@ -10,12 +10,12 @@ read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	sudo systemctl stop AircraftInteresting.service
-	sudo systemctl stop AircraftMilitary.service
-	sudo systemctl disable AircraftInteresting.service
-	sudo systemctl disable AircraftMilitary.service
+	sudo systemctl stop vrtInteresting.service
+	sudo systemctl stop vrtMilitary.service
+	sudo systemctl disable vrtInteresting.service
+	sudo systemctl disable vrtMilitary.service
 	sudo systemctl daemon-reload
 	sudo rm $myhome/repos/VirtualRadarTracker/ -R    
-	sudo rm /etc/systemd/system/Aircraft*.*
+	sudo rm /etc/systemd/system/vrt*.*
 	sudo rm /opt/VirtualRadarTracker/ -R
 fi
